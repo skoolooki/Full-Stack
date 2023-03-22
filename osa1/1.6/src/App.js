9e557a8de4847adbc84c416c90b5461598c21de7
Slava <shukov.sl@gmail.com>
2023-03-22 13:54:14 +0200

    a

--- osa1/1.6/src/App.js
+++ osa1/1.6/src/App.js
@@ -9,13 +9,18 @@ const Button = (props) => {
 }
 
 const Stats = (props) => {
+  const total = props.good + props.neutral + props.bad
+  let average = (props.good - props.bad) / total
+  if (total === 0) {
+    average = 0
+  }
   return (
     <div>
     <p>good: {props.good}</p> 
     <p>neutral: {props.neutral}</p> 
     <p>bad: {props.bad}</p>
-    <p>total: {props.total}</p>
-    <p>average: {props.average}</p>
+    <p>total: {total}</p>
+    <p>average: {average}</p>
     </div> 
   )
 }
@@ -26,27 +31,17 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const [average, setAverage] = useState(0)
 
   const handleGoodClick = () => {
-    setTotal(total + 1)
     setGood(good + 1)
-    setAverage((good - bad) / total)
   }
 
   const handleNeutralClick = () => {
-    console.log(neutral)
-    setTotal(total + 1)
     setNeutral(neutral + 1)
-    setAverage((good - bad) / total)
   }
 
   const handleBadClick = () => {
-    console.log(bad)
-    setTotal(total + 1)
     setBad(bad + 1)
-    setAverage((good - bad) / total)
   }
 
   return (
@@ -56,7 +51,7 @@ const App = () => {
       <Button handleClick={handleNeutralClick} text="neutral"/>
       <Button handleClick={handleBadClick} text="bad"/>
       <h1>Statistics:</h1>
-      <Stats good={good} neutral={neutral} bad={bad} total={total} average={average}/>
+      <Stats good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
