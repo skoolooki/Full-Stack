e353f982cb2f29119e9f8cdf8d9841d9fa0a4119
Slava <shukov.sl@gmail.com>
2023-05-02 10:03:43 +0300

    progreesss

--- osa3/part1/index.js
+++ osa3/part1/index.js
@@ -20,12 +20,15 @@ let notes = [
     }
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
 })
 
 const PORT = 3001
