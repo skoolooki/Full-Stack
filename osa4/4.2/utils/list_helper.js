9692adeddaa2ad01e169cc39eeab2446c4243fd0
Slava <shukov.sl@gmail.com>
2023-11-07 12:56:28 +0200

    4.3 done

--- osa4/4.2/utils/list_helper.js
+++ osa4/4.2/utils/list_helper.js
@@ -2,6 +2,15 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let allLikes = 0
+    blogs.forEach(element => {
+        allLikes = allLikes + element.likes
+    })
+    return allLikes
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }
