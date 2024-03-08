d3fe7b03694f46cafc97b57d80da60f7cfdd8871
slava <shukov.sl@gmail.com>
2024-03-08 13:20:50 +0200

    6.2 toimii

--- osa6/6.1/src/main.jsx
+++ osa6/6.1/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createStore } from 'redux'
+import reducer from './reducer'
+
+const store = createStore(reducer)
+
+const App = () => {
+  const handleClick = (type) => {
+    store.dispatch({ type })
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleClick("GOOD")}>good</button> 
+      <button onClick={() => handleClick("OK")}>ok</button> 
+      <button onClick={() => handleClick("BAD")}>bad</button>
+      <button onClick={() => handleClick("ZERO")}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const renderApp = () => {
+  root.render(<App />)
+}
+
+renderApp()
+store.subscribe(renderApp)
